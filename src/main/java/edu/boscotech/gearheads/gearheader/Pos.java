261e3a4b9819c1c4a463135c18c3a3ba6a7a692f
Lorenzo Pedroza <lorenzo.apedroza2020@gmail.com>
2019-10-14 10:24:51 -0700

    Moved Universal code from 2018-19 into a library I dub Gearheader for Issue #1

---
+++ src/main/java/edu/boscotech/gearheads/gearheader/Pos.java
@@ -0,0 +1,7 @@
+package edu.boscotech.gearheads.gearheader;
+
+public enum Pos {
+     //Consider making this outside the GoldMineral Detector. It may have applications outside of it/and for other seasons.
+        LEFT, MID, RIGHT, UNKNOWN
+
+}
